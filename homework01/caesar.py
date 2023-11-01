be7bdf8c14bf1cb697225ed2464ad5464a7679ec
yana lauzer <73843349+yanalauzer@users.noreply.github.com>
2023-11-01 14:10:05 +0300

    Revert "Homework01"

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,18 +11,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    for i, _ in enumerate(plaintext):
-        if plaintext[i].isalpha():
-            a = ord(plaintext[i])
-            if plaintext[i].isupper() and a >= 91 - shift:
-                ciphertext += chr(a - 26 + shift)
-            elif plaintext[i].islower() and a >= 123 - shift:
-                ciphertext += chr(a - 26 + shift)
-            else:
-                ciphertext += chr(a + shift)
-        else:
-            ciphertext += plaintext[i]
-
+    # PUT YOUR CODE HERE
     return ciphertext
 
 
@@ -39,18 +28,5 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for i, _ in enumerate(ciphertext):
-        if ciphertext[i].isalpha():
-            a = ord(ciphertext[i])
-            if ciphertext[i].isupper() and a <= 64 + shift:
-                plaintext += chr(a + 26 - shift)
-            elif ciphertext[i].islower() and a <= 96 + shift:
-                plaintext += chr(a + 26 - shift)
-            else:
-                plaintext += chr(a - shift)
-        elif ciphertext.isspace():
-            continue
-        else:
-            plaintext += ciphertext[i]
-
+    # PUT YOUR CODE HERE
     return plaintext
