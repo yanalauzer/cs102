03db218a46689056e5b9e6224daf4d9138860d50
Yana <68804461+yaninala@users.noreply.github.com>
2023-11-01 14:27:55 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,8 +12,19 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n <= 0:
+        return False
+    if n == 2:
+        return True
+    elif n == 1:
+        return False
+    if n % 2 == 0:
+        return False
+    d = 3
+    while d ** 2 <= n and n % d != 0:
+        d += 2
+    return d ** 2 > n
+
 
 
 def gcd(a: int, b: int) -> int:
@@ -45,23 +56,23 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
+    n = p*q
     # PUT YOUR CODE HERE
 
     # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
+    e = random.randrange(1, p)
 
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
-    g = gcd(e, phi)
+    g = gcd(e, p)
     while g != 1:
-        e = random.randrange(1, phi)
-        g = gcd(e, phi)
+        e = random.randrange(1, p)
+        g = gcd(e, p)
 
     # Use Extended Euclid's Algorithm to generate the private key
-    d = multiplicative_inverse(e, phi)
+    d = multiplicative_inverse(e, p)
 
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
