41058ca4ebc5d8863514fb6f0d44e85a9fc20e8e
Yana <68804461+yaninala@users.noreply.github.com>
2023-11-15 15:18:43 +0300

    fixess

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -24,7 +24,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
             ciphertext += plaintext[i]
     return ciphertext
 
-
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
